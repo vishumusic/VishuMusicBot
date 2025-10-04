6053c2fc3daa0e72542e1d5903761c985f8ec531
ARYANSONIOP <soniaryan2240017@gmail.com>
2025-10-04 14:25:35 +0530

    Update watcher.py

--- SmartMusic/plugins/misc/watcher.py
+++ SmartMusic/plugins/misc/watcher.py
@@ -1,8 +1,8 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from BrandrdXMusic import app
-from BrandrdXMusic.core.call import Hotty
+from SmartMusic import app
+from SmartMusic.core.call import Hotty
 
 welcome = 20
 close = 30
