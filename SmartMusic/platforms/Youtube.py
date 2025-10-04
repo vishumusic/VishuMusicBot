039b0efd65cbd33d13d57bd1d2c53c0cde500730
ARYANSONIOP <soniaryan2240017@gmail.com>
2025-10-04 13:51:07 +0530

    Update Youtube.py

--- SmartMusic/platforms/Youtube.py
+++ SmartMusic/platforms/Youtube.py
@@ -8,8 +8,8 @@ import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from youtubesearchpython.__future__ import VideosSearch
-from BrandrdXMusic.utils.database import is_on_off
-from BrandrdXMusic.utils.formatters import time_to_seconds
+from SmartMusic.utils.database import is_on_off
+from SmartMusic.utils.formatters import time_to_seconds
 import os
 import glob
 import random
