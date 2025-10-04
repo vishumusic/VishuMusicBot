2c2d14dcbeeec26d5709c0f51860f0f84f08f442
ARYANSONIOP <soniaryan2240017@gmail.com>
2025-10-04 14:32:37 +0530

    Update maintenance.py

--- SmartMusic/plugins/sudo/maintenance.py
+++ SmartMusic/plugins/sudo/maintenance.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from BrandrdXMusic import app
+from SmartMusic import app
 from config import OWNER_ID
-from BrandrdXMusic.utils.database import (
+from SmartMusic.utils.database import (
     get_lang,
     is_maintenance,
     maintenance_off,
