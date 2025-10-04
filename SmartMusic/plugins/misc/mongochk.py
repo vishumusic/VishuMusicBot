9ccbe598ca8e2d266bedb3a4d7ae0822b51bb70c
ARYANSONIOP <soniaryan2240017@gmail.com>
2025-10-04 14:23:54 +0530

    Update mongochk.py

--- SmartMusic/plugins/misc/mongochk.py
+++ SmartMusic/plugins/misc/mongochk.py
@@ -2,7 +2,7 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from pymongo import MongoClient
 import re
-from BrandrdXMusic import app as Hotty
+from SmartMusic import app as Hotty
 
 
 mongo_url_pattern = re.compile(r'mongodb(?:\+srv)?:\/\/[^\s]+')
