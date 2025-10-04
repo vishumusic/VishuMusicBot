31cca546eeaa8aaf236487c59b14449e4765c9e5
ARYANSONIOP <soniaryan2240017@gmail.com>
2025-10-04 14:07:29 +0530

    Update __main__.py

--- SmartMusic/__main__.py
+++ SmartMusic/__main__.py
@@ -5,11 +5,11 @@ from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
-from BrandrdXMusic import LOGGER, app, userbot
-from BrandrdXMusic.core.call import Hotty
-from BrandrdXMusic.misc import sudo
-from BrandrdXMusic.plugins import ALL_MODULES
-from BrandrdXMusic.utils.database import get_banned_users, get_gbanned
+from SmartMusic import LOGGER, app, userbot
+from SmartMusic.core.call import Hotty
+from SmartMusic.misc import sudo
+from SmartMusic.plugins import ALL_MODULES
+from SmartMusic.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
 
 
@@ -35,27 +35,27 @@ async def init():
         pass
     await app.start()
     for all_module in ALL_MODULES:
-        importlib.import_module("BrandrdXMusic.plugins" + all_module)
-    LOGGER("BrandrdXMusic.plugins").info("Successfully Imported Modules...")
+        importlib.import_module("SmartMusic.plugins" + all_module)
+    LOGGER("SmartMusic.plugins").info("Successfully Imported Modules...")
     await userbot.start()
     await Hotty.start()
     try:
         await Hotty.stream_call("https://graph.org/file/e999c40cb700e7c684b75.mp4")
     except NoActiveGroupCall:
-        LOGGER("BrandrdXMusic").error(
+        LOGGER("SmartMusic").error(
             "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
         )
         exit()
     except:
         pass
     await Hotty.decorators()
-    LOGGER("BrandrdXMusic").info(
+    LOGGER("SmartMusic").info(
         "ᴅʀᴏᴘ ʏᴏᴜʀ ɢɪʀʟꜰʀɪᴇɴᴅ'ꜱ ɴᴜᴍʙᴇʀ ᴀᴛ @smartness_to_hai ᴊᴏɪɴ @tabahi_tabahi , @tabahi_tabahi ꜰᴏʀ ᴀɴʏ ɪꜱꜱᴜᴇꜱ"
     )
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("BrandrdXMusic").info("Stopping Brandrd Music Bot...")
+    LOGGER("SmartMusic").info("Stopping Brandrd Music Bot...")
 
 
 if __name__ == "__main__":
