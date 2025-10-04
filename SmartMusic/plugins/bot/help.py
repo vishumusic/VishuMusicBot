4f01bb2f70580d447747b1aefa478fc0ffde0f88
ARYANSONIOP <soniaryan2240017@gmail.com>
2025-10-04 14:13:25 +0530

    Update help.py

--- SmartMusic/plugins/bot/help.py
+++ SmartMusic/plugins/bot/help.py
@@ -3,11 +3,11 @@ from typing import Union
 from pyrogram import filters, types
 from pyrogram.types import InlineKeyboardMarkup, Message, InlineKeyboardButton
 
-from BrandrdXMusic import app
-from BrandrdXMusic.utils import help_pannel
-from BrandrdXMusic.utils.database import get_lang
-from BrandrdXMusic.utils.decorators.language import LanguageStart, languageCB
-from BrandrdXMusic.utils.inline.help import help_back_markup, private_help_panel
+from SmartMusic import app
+from SmartMusic.utils import help_pannel
+from SmartMusic.utils.database import get_lang
+from SmartMusic.utils.decorators.language import LanguageStart, languageCB
+from SmartMusic.utils.inline.help import help_back_markup, private_help_panel
 from config import BANNED_USERS, START_IMG_URL, SUPPORT_CHAT
 from strings import get_string, helpers
 
