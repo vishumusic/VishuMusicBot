b9a985c2ab730315b0f6c1eb6a456a05b7d197f3
ARYANSONIOP <soniaryan2240017@gmail.com>
2025-10-04 15:20:09 +0530

    Update database.py

--- SmartMusic/utils/database.py
+++ SmartMusic/utils/database.py
@@ -191,7 +191,7 @@ async def set_assistant_new(chat_id, number):
 
 
 async def set_assistant(chat_id):
-    from BrandrdXMusic.core.userbot import assistants
+    from SmartMusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -205,7 +205,7 @@ async def set_assistant(chat_id):
 
 
 async def get_assistant(chat_id: int) -> str:
-    from BrandrdXMusic.core.userbot import assistants
+    from SmartMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -232,7 +232,7 @@ async def get_assistant(chat_id: int) -> str:
 
 
 async def set_calls_assistant(chat_id):
-    from BrandrdXMusic.core.userbot import assistants
+    from SmartMusic.core.userbot import assistants
 
     ran_assistant = random.choice(assistants)
     assistantdict[chat_id] = ran_assistant
@@ -245,7 +245,7 @@ async def set_calls_assistant(chat_id):
 
 
 async def group_assistant(self, chat_id: int) -> int:
-    from BrandrdXMusic.core.userbot import assistants
+    from SmartMusic.core.userbot import assistants
 
     assistant = assistantdict.get(chat_id)
     if not assistant:
@@ -980,4 +980,5 @@ async def add_served_chat_clone(chat_id: int):
 
 async def delete_served_chat_clone(chat_id: int):
     await chatsdbc.delete_one({"chat_id": chat_id})
+
 
