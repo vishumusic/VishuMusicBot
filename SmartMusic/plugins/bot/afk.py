90ed4ea1870a7eaa9befd7b6febc5f035ec7a78c
ARYANSONIOP <soniaryan2240017@gmail.com>
2025-10-04 14:12:54 +0530

    Update afk.py

--- SmartMusic/plugins/bot/afk.py
+++ SmartMusic/plugins/bot/afk.py
@@ -2,9 +2,9 @@ import time, re
 from pyrogram.enums import MessageEntityType
 from pyrogram import filters
 from pyrogram.types import Message
-from BrandrdXMusic import app
-from BrandrdXMusic.mongo.readable_time import get_readable_time
-from BrandrdXMusic.mongo.afkdb import add_afk, is_afk, remove_afk
+from SmartMusic import app
+from SmartMusic.mongo.readable_time import get_readable_time
+from SmartMusic.mongo.afkdb import add_afk, is_afk, remove_afk
 
 
 @app.on_message(filters.command(["afk", "brb"], prefixes=["/", "!"]))
