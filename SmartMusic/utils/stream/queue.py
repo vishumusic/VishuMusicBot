26e9f6657eaa170639a2c803e322b3ce69c9a45b
ARYANSONIOP <soniaryan2240017@gmail.com>
2025-10-04 13:59:19 +0530

    Update queue.py

--- SmartMusic/utils/stream/queue.py
+++ SmartMusic/utils/stream/queue.py
@@ -1,8 +1,8 @@
 import asyncio
 from typing import Union
 
-from BrandrdXMusic.misc import db
-from BrandrdXMusic.utils.formatters import check_duration, seconds_to_min
+from SmartMusic.misc import db
+from SmartMusic.utils.formatters import check_duration, seconds_to_min
 from config import autoclean, time_to_seconds
 
 
