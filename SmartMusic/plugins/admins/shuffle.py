863610eea286f5d71bd91811754df6aa6244899a
ARYANSONIOP <soniaryan2240017@gmail.com>
2025-10-04 14:11:12 +0530

    Update shuffle.py

--- SmartMusic/plugins/admins/shuffle.py
+++ SmartMusic/plugins/admins/shuffle.py
@@ -3,10 +3,10 @@ import random
 from pyrogram import filters
 from pyrogram.types import Message
 
-from BrandrdXMusic import app
-from BrandrdXMusic.misc import db
-from BrandrdXMusic.utils.decorators import AdminRightsCheck
-from BrandrdXMusic.utils.inline import close_markup
+from SmartMusic import app
+from SmartMusic.misc import db
+from SmartMusic.utils.decorators import AdminRightsCheck
+from SmartMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
