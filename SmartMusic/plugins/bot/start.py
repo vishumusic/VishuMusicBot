78a7c86ddba941833e6dfe575a53eddae3b4563a
ARYANSONIOP <soniaryan2240017@gmail.com>
2025-10-04 14:20:32 +0530

    Update start.py

--- SmartMusic/plugins/bot/start.py
+++ SmartMusic/plugins/bot/start.py
@@ -6,10 +6,10 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
 
 import config
-from BrandrdXMusic import app
-from BrandrdXMusic.misc import _boot_
-from BrandrdXMusic.plugins.sudo.sudoers import sudoers_list
-from BrandrdXMusic.utils.database import (
+from SmartMusic import app
+from SmartMusic.misc import _boot_
+from SmartMusic.plugins.sudo.sudoers import sudoers_list
+from SmartMusic.utils.database import (
     add_served_chat,
     add_served_user,
     blacklisted_chats,
@@ -17,9 +17,9 @@ from BrandrdXMusic.utils.database import (
     is_banned_user,
     is_on_off,
 )
-from BrandrdXMusic.utils.decorators.language import LanguageStart
-from BrandrdXMusic.utils.formatters import get_readable_time
-from BrandrdXMusic.utils.inline import help_pannel, private_panel, start_panel
+from SmartMusic.utils.decorators.language import LanguageStart
+from SmartMusic.utils.formatters import get_readable_time
+from SmartMusic.utils.inline import help_pannel, private_panel, start_panel
 from config import BANNED_USERS
 from strings import get_string
 
