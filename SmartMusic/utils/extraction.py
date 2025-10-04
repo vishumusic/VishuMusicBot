fae6876e100c7c3408a540b6c4186f3bfa4f70f2
ARYANSONIOP <soniaryan2240017@gmail.com>
2025-10-04 14:03:08 +0530

    Update extraction.py

--- SmartMusic/utils/extraction.py
+++ SmartMusic/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from BrandrdXMusic import app
+from SmartMusic import app
 
 
 async def extract_user(m: Message) -> User:
