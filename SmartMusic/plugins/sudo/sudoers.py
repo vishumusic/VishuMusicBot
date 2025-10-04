b93aa56a53e5b75d9daebc4c838d98f4b312265e
ARYANSONIOP <soniaryan2240017@gmail.com>
2025-10-04 14:33:41 +0530

    Update sudoers.py

--- SmartMusic/plugins/sudo/sudoers.py
+++ SmartMusic/plugins/sudo/sudoers.py
@@ -1,12 +1,12 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from BrandrdXMusic import app
-from BrandrdXMusic.misc import SUDOERS
-from BrandrdXMusic.utils.database import add_sudo, remove_sudo
-from BrandrdXMusic.utils.decorators.language import language
-from BrandrdXMusic.utils.extraction import extract_user
-from BrandrdXMusic.utils.inline import close_markup
+from SmartMusic import app
+from SmartMusic.misc import SUDOERS
+from SmartMusic.utils.database import add_sudo, remove_sudo
+from SmartMusic.utils.decorators.language import language
+from SmartMusic.utils.extraction import extract_user
+from SmartMusic.utils.inline import close_markup
 from config import BANNED_USERS, OWNER_ID
 
 
