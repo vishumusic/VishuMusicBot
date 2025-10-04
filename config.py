0a1d92e13b347a61a18c6c6cc470dcaf8fde0772
vishumusic <vsh11547@gmail.com>
2025-10-04 18:48:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ARYANSONIOP/NewMusicBot",
+    "https://github.com/vishumusic/VishuMusicBot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
